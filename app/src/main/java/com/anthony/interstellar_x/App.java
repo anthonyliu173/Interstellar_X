8015369d926e0304898e9b89ef721630a154a771
Anthony Liu <ymliu173@gmail.com>
2016-04-03 11:37:15 +0800

    first commit, project setup

---
+++ app/src/main/java/com/anthony/interstellar_x/App.java
@@ -0,0 +1,16 @@
+package com.anthony.interstellar_x;
+
+import android.app.Application;
+
+/**
+ * Created by anthonyliu on 2016/4/3.
+ */
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+}
