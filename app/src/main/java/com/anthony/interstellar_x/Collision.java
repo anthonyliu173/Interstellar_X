baf39eb55eb2cb68020fe7d963479bc706aeb923
Anthony Liu <ymliu173@gmail.com>
2016-04-06 09:52:22 +0800

    work on collision bounce formula

--- app/src/main/java/com/anthony/interstellar_x/Collision.java
+++ app/src/main/java/com/anthony/interstellar_x/Collision.java
@@ -1,9 +1,7 @@
 package com.anthony.interstellar_x;
 
-import com.anthony.interstellar_x.Interstellar_Objects.Blackhole;
 import com.anthony.interstellar_x.Interstellar_Objects.PhysicalObject;
 
-import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -13,13 +11,13 @@ public class Collision {
 
     public static void collideAnalysis(List<PhysicalObject> physicalObjects){
 
-        // Remove blackholes from collide analysis
-        for (Iterator<PhysicalObject> iter = physicalObjects.listIterator(); iter.hasNext(); ) {
-            PhysicalObject physicalObject = iter.next();
-            if (physicalObject instanceof Blackhole) {
-                iter.remove();
-            }
-        }
+//        // Remove blackholes from collide analysis
+//        for (Iterator<PhysicalObject> iter = physicalObjects.listIterator(); iter.hasNext(); ) {
+//            PhysicalObject physicalObject = iter.next();
+//            if (physicalObject instanceof Blackhole) {
+//                iter.remove();
+//            }
+//        }
 
         for(int i = 0; i < physicalObjects.size(); i++){
             // All but last physicalObject is processed
@@ -28,6 +26,7 @@ public class Collision {
                     if(isColliding(physicalObjects.get(i), physicalObjects.get(j))){
                         System.out.println("COLLIDING!!!");
                         //TODO Update velocities of two physicalObjects
+                        velocityExchange(physicalObjects.get(i), physicalObjects.get(j));
                     }
                 }
             }
@@ -50,6 +49,27 @@ public class Collision {
         }
 
         return isColliding;
+    }
+
+    private static void velocityExchange(PhysicalObject object1, PhysicalObject object2){
+        double M1 = getM1(object1.getMass(), object2.getMass());
+        double M2 = getM2(object1.getMass(), object2.getMass());
+        double v1_x = object1.getVelocity_x();
+        double v1_y = object1.getVelocity_y();
+        double v2_x = object2.getVelocity_x();
+        double v2_y = object2.getVelocity_y();
+        object1.setVelocity_x(M2*v1_x + M1*v2_x);
+        object1.setVelocity_y(M2 * v1_y + M1 * v2_y);
+        object2.setVelocity_x(M1*v1_x - M2*v2_x);
+        object2.setVelocity_y(M1*v1_y - M2*v2_y);
+    }
+
+    private static double getM1(int mass_1, int mass_2){
+        return ((double)2*mass_1)/(mass_1 + mass_2);
+    }
+
+    private static double getM2(int mass_1, int mass_2){
+        return ((double)(mass_1 - mass_2))/(mass_1 + mass_2);
     }
 
 }
