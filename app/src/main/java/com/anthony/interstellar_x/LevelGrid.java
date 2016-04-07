ff53cb0e66cffa8e01e59b761ed0d69e67733764
Anthony Liu <ymliu173@gmail.com>
2016-04-07 16:10:08 +0800

    work on adapter of the MainActivity

---
+++ app/src/main/java/com/anthony/interstellar_x/LevelGrid.java
@@ -0,0 +1,64 @@
+package com.anthony.interstellar_x;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by anthonyliu on 2016/4/7.
+ */
+public class LevelGrid extends BaseAdapter {
+
+    private Context mContext;
+    private final String[] levels;
+
+    public LevelGrid(Context context,String[] levels) {
+        mContext = context;
+        this.levels = levels;
+    }
+
+    @Override
+    public int getCount() {
+        // TODO Auto-generated method stub
+        return levels.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // TODO Auto-generated method stub
+        View grid;
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+
+            grid = new View(mContext);
+            grid = inflater.inflate(R.layout.grid_single, null);
+            TextView textView = (TextView) grid.findViewById(R.id.grid_text);
+            ImageView imageView = (ImageView)grid.findViewById(R.id.grid_image);
+            textView.setText(web[position]);
+            imageView.setImageResource(Imageid[position]);
+        } else {
+            grid = (View) convertView;
+        }
+
+        return grid;
+    }
+
+}
